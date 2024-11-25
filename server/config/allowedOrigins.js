1da3a8cb4a0888a1e6e9a3d7312e54b5301d28b5
lokeshklu <lokeshchowdary957@gmail.com>
2024-11-25 15:30:47 -0800

    Update package-lock.json to resolve subdependency warnings

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,8 +1,7 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const allowedOrigins = [
-    'http://localhost:3000',
-    process.env.BASE_URL_PROD // Add your frontend origin here
-]
+  process.env.BASE_URL_PROD, // Add your frontend origin here
+];
 
-module.exports = allowedOrigins
+module.exports = allowedOrigins;
